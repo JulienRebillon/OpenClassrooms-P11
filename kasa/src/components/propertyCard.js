fcb172a6fcf255690f76991130f395c51c849c9b
JulienRebillon <julienrebillon@yahoo.fr>
2024-08-25 23:21:57 +0200

    fix rating stars.

--- kasa/src/components/propertyCard.js
+++ kasa/src/components/propertyCard.js
@@ -1,40 +1,8 @@
-// import React, { useState } from "react";
-
-// import '../styles/propertyCard.css';
-
-
-// const PropertyCard = ({ coverImageUrl, title, location, hostName, hostSurname, hostPictureUrl }) => {
-//     return (
-//       <div className="property-container">
-//         {/* Cover Image */}
-//         <img src={coverImageUrl} alt="Property Cover" className="cover-image" />
-  
-//         {/* Flex Container */}
-//         <div className="details-container">
-//           {/* Left Side: Title and Location */}
-//           <div className="left-side">
-//             <h1 className="property-title">{title}</h1>
-//             <p className="property-location">{location}</p>
-//           </div>
-  
-//           {/* Right Side: Host Info */}
-//           <div className="right-side">
-//             <div className="host-info">
-//               <p className="host-name">{hostName}</p>
-//               <p className="host-name">{hostSurname}</p>
-//               <img src={hostPictureUrl} alt="Host" className="host-picture" />
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   };
-
-
-// export default PropertyCard;
-
 import React from 'react';
 import '../styles/propertyCard.css';
+import Collapse from './collapse';
+import StarFull from '../images/Star-full.png';
+import StarEmpty from '../images/Star-empty.png';
 
 function PropertyCard({ title, location, host, rating, description, equipments, tags }) {
     // Convert rating to an array to render stars
@@ -56,26 +24,34 @@ function PropertyCard({ title, location, host, rating, description, equipments, 
                     <div className="host-name">{host.name}</div>
                     <img className="host-picture" src={host.picture} alt={`${host.name}`} />
                 </div>
+                
+                <div className="property-rating">
+                    {ratingStars.map((isFilled, index) => (
+                        <img 
+                            key={index} 
+                            src={isFilled ? StarFull : StarEmpty} 
+                            alt={isFilled ? "Full Star" : "Empty Star"} 
+                            className="star"
+                        />
+                    ))}
+                </div>
+
             </div>
 
-            <div className="property-rating">
-                {ratingStars.map((isFilled, index) => (
-                    <span key={index} className={`star ${isFilled ? 'filled' : 'empty'}`}>★</span>
-                ))}
-            </div>
+            
 
             <div className="property-description">
-                <p>{description}</p>
+                {/* Pass description as a string to the content prop */}
+                <Collapse title="Description" content={description} />
             </div>
 
             <div className="property-equipments">
-                <h3>Équipements</h3>
-                <ul>
-                    {equipments.map((equipment, index) => (
-                        <li key={index}>{equipment}</li>
-                    ))}
-                </ul>
+                {/* Pass equipments array directly to the content prop */}
+                <Collapse title="Équipements" content={equipments} />
             </div>
+          
+
+            
         </div>
     );
 }
